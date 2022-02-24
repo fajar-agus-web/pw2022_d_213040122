fa151d1370d4aa6b99a0581b7e440cfe10e15ec2
fajar-agus-web <fajar.agus.web@gmail.com>
2022-02-24 11:28:17 +0700

    mengerjakan tugas pertemuan 2

---
+++ kuliah/pertemuan 2/latihan1.php
@@ -0,0 +1,47 @@
+<?php
+// Pertemuan 2. belajar mengenal sintaks php
+// echo untul mencetak nilai ke layar
+echo (1 + 2) * 3 / 4;
+
+echo "<hr>";
+
+// OPERATOR
+// Aritmatika
+// +, -, /, *, %
+
+echo "<hr>";
+echo 5 + 2;
+
+// Perbandingan
+// <, >, <=, >=, ==, dll
+echo "<hr>";
+echo 10 > 2;
+
+// Variabel
+// Tempat menampung nilai
+// Tidak boleh mengandung spasi
+// boleh mengandung angka, tidak boleh diawal
+// snake_case : $nama_depan_mahasiswa
+// camelCase : namaDepanMahasiswa
+// kebab-case : nama-depan-mahasiswa
+$nama = 'fajar';
+echo $nama;
+
+
+// Penugasan / Assignment
+//  =, +=. -=, *=, /=, %=
+$a = 10;
+$a = 20;
+$a = 30;
+echo $a;
+
+
+// Increment & Decrement
+// ++, --
+$b = 10;
+echo $b++;
+echo $b;
+
+// identitas
+// ===, !==
+echo 10 === "10";
